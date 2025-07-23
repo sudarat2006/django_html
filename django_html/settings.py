d4d929bf213a83246c49c15676c23b4dad826722
sudarat2006 <sudarat.wanthawee@gmail.com>
2025-07-23 16:13:03 +0700

    Comment out database configuration in settings.py

--- django_html/settings.py
+++ django_html/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'django_html.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
