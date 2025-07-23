52c1dcd640d950ffdbf73fbc68c7266aacff9667
sudarat2006 <sudarat.wanthawee@gmail.com>
2025-07-23 16:20:42 +0700

    Add compatibility alias for WSGI application callable

--- django_html/wsgi.py
+++ django_html/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_html.settings')
 
 application = get_wsgi_application()
+app = application  # For compatibility with some WSGI servers that expect 'app' to be the callable
